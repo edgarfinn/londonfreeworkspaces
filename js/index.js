a6c8ae6888b359409132cfcff01ac1162e8b6a74
Edgar Dewsbery <edewsbery@gmail.com>
2016-11-25 18:05:04 +0000

    initial v2 commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function selfcheck(clicked_id) {
+  if (document.getElementById(clicked_id).checked == false) {
+    document.getElementById(clicked_id).checked = true;    
+  }
+   else if (document.getElementById(clicked_id).checked == true) {
+    document.getElementById(clicked_id).checked = false;    
+  }
+}
